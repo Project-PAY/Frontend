28ddd68eb8cf3a121318f807bacec9186698f8ba
Pewww <jung22337@naver.com>
2019-09-10 01:06:31 +0900

    :lipstick: index.tsx의 css import 구문 제거 및 styled-components의 createGlobalStyle 사용

--- pay/src/index.tsx
+++ pay/src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
@@ -9,6 +8,21 @@ import reducers from './reducers';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import routes from '../pages/routes/routes';
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body > div {
+    height: 100%;
+  }
+`;
 
 const store = createStore(reducers, composeWithDevTools(
   applyMiddleware(thunk)
@@ -17,6 +31,7 @@ const store = createStore(reducers, composeWithDevTools(
 ReactDOM.render(
   <Provider store={store}>
     <App routes={routes}/>
+    <GlobalStyle/>
   </Provider>,
   document.getElementById('root')
 );
