64c7fca72e8f202bcf93b1d22335e0fca97ef0a1
Pewww <jung22337@naver.com>
2019-09-09 16:57:21 +0900

    :pencil: anonRequired hoc에 필요 주석 추가

--- pay/hocs/anonRequired.tsx
+++ pay/hocs/anonRequired.tsx
@@ -4,6 +4,7 @@ import {IRootState} from '../src/reducers';
 import {useSelector} from 'react-redux';
 import isEqual from 'lodash/isEqual';
 
+// @TODO: 페이지 뒤로 이동이 아닌 메인페이지로 이동되도록 코드 수정
 const anonRequired = <T extends {}>(Target: React.ComponentType) => {
   const AnonRequired = ({history, ...props}: T & RouteComponentProps) => {
     const {session: {access}} = useSelector(
--- pay/pages/index.tsx
+++ pay/pages/index.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
+import {RouteComponentProps} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {IRootState} from '../src/reducers';
 
-const Main = () => {
+interface Props extends RouteComponentProps {
+}
+
+const Main = ({history}: Props) => {
+  const {session: {access}} = useSelector(({system}: IRootState) => system);
+
   return (
     <div>Hello</div>
   );
