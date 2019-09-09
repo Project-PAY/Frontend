2af25b49498da2862bef7de6d98c55fadf3bdda9
Pewww <jung22337@naver.com>
2019-09-09 17:30:36 +0900

    :lipstick: splash 페이지 내부 div와 span styled-components 적용 및 styled 폴더 추가
    색 관련 변수들을 저장하는 variables.types.ts 파일 생성

--- pay/pages/splash/index.tsx
+++ pay/pages/splash/index.tsx
@@ -2,15 +2,32 @@ import * as React from 'react';
 import {RouteComponentProps} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {IRootState} from '../../src/reducers';
+import styled from 'styled-components';
+import {SECOND} from '../../src/constants/times';
+
+const Div = styled.div`
+`;
+
+const Span = styled.span`
+`;
 
 interface Props extends RouteComponentProps {
 }
 
 const Splash: React.FC<Props> = ({history}) => {
   const {session: {access}} = useSelector(({system}: IRootState) => system);
+  
+  React.useEffect(() => {
+    setTimeout(() => {
+      const to = access ? '/main' : '/welcome';
+      history.replace(to);
+    }, 2.5 * SECOND);
+  }, []);
 
   return (
-    <div>Splash</div>
+    <Div>
+      <Span>PAY</Span>
+    </Div>
   );
 };
 
---
+++ pay/styles/variables.types.ts
@@ -0,0 +1,4 @@
+export const $WHTIE = '#fff';
+export const $MAIN_COLOR = '#6c24e8';
+export const $BLACK = '#000';
+export const $BORDER_COLOR = '#e0e0e0';
