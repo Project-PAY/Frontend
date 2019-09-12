c1f052f0b9732f487fae91c029f950d674241a64
Pewww <jung22337@naver.com>
2019-09-12 22:48:55 +0900

    :sparkles: TitleComp 컴포넌트 추가 및 base.styles.ts에서 일부 공통 코드 제거

--- pay/pages/welcome/index.tsx
+++ pay/pages/welcome/index.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import {$MAIN} from '../../styles/variables.types';
+import TitleComp from '../../src/components/common/TitleComp';
+
+const Div = styled.div`
+  height: 100%;
+  background-color: ${$MAIN};
+`;
 
 const Welcome = () => {
   return (
-    <div>Welcome Page</div>
+    <Div>
+      <TitleComp/>
+    </Div>
   );
 };
 
---
+++ pay/src/components/common/TitleComp.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import styled, {css} from 'styled-components';
+import {fontStyleMixin} from '../../../styles/mixins.styles';
+import {$WHTIE} from '../../../styles/variables.types';
+
+const commonHeadingStyle = (size: number) => (
+  css`
+    ${fontStyleMixin({
+      family: 'Gotham-Bold',
+      color: $WHTIE,
+      size
+    })};
+    padding-left: 30px;
+  `
+);
+
+const H1 = styled.h1`
+  margin-top: 0;
+  padding-top: 90px;
+  letter-spacing: -2px;
+  ${commonHeadingStyle(40)};
+`;
+
+const H2 = styled.h2`
+  opacity: .5;
+  letter-spacing: -0.5px;
+  ${commonHeadingStyle(20)};
+`;
+
+interface Props {
+  className?: string;
+}
+
+const TitleComp: React.FC<Props> = React.memo(({className}) => (
+  <div className={className}>
+    <H1>PAY</H1>
+    <H2>How much you spent</H2>
+  </div>
+));
+
+export default TitleComp;
