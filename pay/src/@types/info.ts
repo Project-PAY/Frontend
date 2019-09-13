70a792b8f083799497e802baa7651b918c8dbc16
Pewww <jung22337@naver.com>
2019-09-13 22:11:35 +0900

    :sparkles: InfoApi 추가

--- pay/src/@types/info.ts
+++ pay/src/@types/info.ts
@@ -1,6 +1,10 @@
-export interface InfoState {
+export interface IBaseInfo {
   current_money: number;
   has_fixed_income: boolean;
   fixed_income?: number;
   income_cycle?: number;
+}
+
+export interface InfoState extends IBaseInfo {
+
 }
---
+++ pay/src/apis/InfoApi.ts
@@ -0,0 +1,22 @@
+import Api from './base/Api';
+import {TokenType} from '../@types/auth';
+import {IBaseInfo} from '../@types/info';
+
+class InfoApi extends Api {
+  constructor(token?: TokenType) {
+    super({
+      token,
+      model: 'info'
+    }); 
+  }
+
+  getInfo() {
+    return this.retrieve('');
+  }
+
+  setBaseInfo(form: IBaseInfo) {
+    return this.getAxios().post(`/${this.model}/`, form);
+  }
+}
+
+export default InfoApi;
