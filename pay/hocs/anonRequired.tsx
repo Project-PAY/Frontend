83aa58f3b3104758f3e894e0bfc02391114cc568
Pewww <jung22337@naver.com>
2019-09-19 00:44:55 +0900

    :zap: anonRequired hoc에서 props로 history도 넘기도록 코드 수정

--- pay/hocs/anonRequired.tsx
+++ pay/hocs/anonRequired.tsx
@@ -6,7 +6,7 @@ import isEqual from 'lodash/isEqual';
 
 // @TODO: AnonRequired any 타입 수정
 const anonRequired = <T extends {}>(Target: React.FC<T>) => {
-  const AnonRequired = ({history, ...props}: /* T & RouteComponentProps */ any) => {
+  const AnonRequired = ({...props}: /* T & RouteComponentProps */ any) => {
     const {session: {access}} = useSelector(
       ({system}: IRootState) => system,
       (prev, curr) => isEqual(prev, curr)
@@ -14,7 +14,7 @@ const anonRequired = <T extends {}>(Target: React.FC<T>) => {
 
     if (access) {
       alert('이미 로그인 하였습니다.');
-      history.replace('/main');
+      props.history.replace('/main');
     }
 
     return <Target {...props}/>;
