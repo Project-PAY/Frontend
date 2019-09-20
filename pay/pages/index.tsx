2d750b2ba63427aa724f32c751ceddac0dbf3c41
Pewww <jung22337@naver.com>
2019-09-20 23:56:04 +0900

    :lipstick: 메인 페이지중 Header 및 Ul 기본 스타일 적용

--- pay/pages/index.tsx
+++ pay/pages/index.tsx
@@ -1,13 +1,34 @@
 import * as React from 'react';
 import OGMetaHead from '../src/components/common/OGMetaHead';
+import styled from 'styled-components';
+import {$MAIN, $WHTIE} from '../styles/variables.types';
+
+const Div = styled.div`
+  height: 100%;
+`;
+
+const Header = styled.header`
+  position: relative;
+  height: 240px;
+  background-color: ${$MAIN};
+  padding-left: 30px;
+`;
+
+const Ul = styled.ul`
+  height: calc(100% - 240px);
+  background-color: ${$WHTIE}
+`;
 
 // @TODO: loginRequired hoc 적용
 const Main = () => {
   return (
-    <div>
+    <Div>
       <OGMetaHead title="메인페이지"/>
-      Hello
-    </div>
+      <Header>
+      </Header>
+      <Ul>
+      </Ul>
+    </Div>
   );
 };
 
