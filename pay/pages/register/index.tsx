8a611d35c625e6003a5798874531186a277a603c
Pewww <jung22337@naver.com>
2019-09-29 16:53:41 +0900

    :sparkles: 수입/지출 등록 페이지에 뒤로 가기 아이콘 추가

--- pay/pages/register/index.tsx
+++ pay/pages/register/index.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import {$MAIN} from '../../styles/variables.types';
+import goBack from '../../src/assets/icons/icon-go-back.png';
+
+const Div = styled.div`
+  height: 100%;
+  background-color: ${$MAIN};
+`;
+
+const GoBackImg = styled.img`
+  position: absolute;
+  width: 29px;
+  top: 60px;
+  left: 30px;
+  cursor: pointer;
+`;
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <Div>
+      <GoBackImg
+        src={goBack}
+        alt="뒤로가기"
+      />
+    </Div>
   );
 };
 
